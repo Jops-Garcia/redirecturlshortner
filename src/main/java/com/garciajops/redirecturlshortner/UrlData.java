d2ee5c43fd10ce8c0f38adee1e6caac59ad61d87
Jops-Garcia <garcia.jops@gmail.com>
2024-11-21 19:56:34 -0300

    adicionar arquivos

---
+++ src/main/java/com/garciajops/redirecturlshortner/UrlData.java
@@ -0,0 +1,15 @@
+package com.garciajops.redirecturlshortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
